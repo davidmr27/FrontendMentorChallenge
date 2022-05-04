a2341520bd7b5c2fc087a45d3714a5e0e6b40480
vidm27 <davidmoran464@gmail.com>
2022-05-04 04:16:44 -0500

    Sidebar ocultar y mostrar

--- in-browser-markdown-editor/src/js/main.js
+++ in-browser-markdown-editor/src/js/main.js
@@ -1,42 +1,46 @@
 var icons = {
     eye: '<i class="fa-solid fa-eye"></i>',
     eyeSlash: '<i class="fa-solid fa-eye-slash"></i>',
-    menuOpen:  '<i class="fa-solid fa-bars"></i>',
+    menuOpen: '<i class="fa-solid fa-bars"></i>',
     menuClose: '<i class="fa-solid fa-xmark"></i>'
 }
 let menu = document.getElementById('menu');
 
 menu.addEventListener('click', (e) => {
     let firstChild = menu.firstChild;
+    let layout = document.getElementById('layout');
+    let sidenav = document.getElementById('sidenav');
 
-    if(menu.classList.contains('close')){
+    if (menu.classList.contains('close')) {
         menu.classList.remove('close');
         menu.removeChild(firstChild);
         menu.innerHTML = icons.menuClose;
         menu.classList.add('open');
-    }else if(menu.classList.contains('open')){
+    } else if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         menu.removeChild(firstChild);
         menu.innerHTML = icons.menuOpen;
         menu.classList.add('close');
     }
+    layout.classList.toggle('sidenav-close');
+    sidenav.classList.toggle('sidenav-close');
 })
 
 // hide and show editor and preview
 let btnPreview = document.getElementById('btn-preview');
-btnPreview.addEventListener('click', (e) =>{
-    // se maximiza el preview
-    // se oculta el editor
-    let iconEyesSlash = '<i class="fa-solid fa-eye-slash"></i>';
+btnPreview.addEventListener('click', (e) => {
     let editor = document.getElementById('editor');
-    if(btnPreview.classList.contains('preview-open')){
+    let preview = document.getElementById('preview');
+
+    if (btnPreview.classList.contains('preview-open')) {
         editor.style.width = '0';
         btnPreview.removeChild(btnPreview.firstChild);
         btnPreview.innerHTML = icons.eyeSlash;
-    }else{
+    } else {
         editor.style.width = '100%';
         btnPreview.removeChild(btnPreview.firstChild);
         btnPreview.innerHTML = icons.eye;
     }
     btnPreview.classList.toggle('preview-open');
+    preview.classList.toggle('content-center');
 });
